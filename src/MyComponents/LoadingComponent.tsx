679ab5c3cf28eeb6ed34a2438c2e5c11a171f168
Steven Chorkawy <schorkawy@clarington.net>
2024-01-12 10:16:31 -0500

    Creating a loading component.

---
+++ src/MyComponents/LoadingComponent.tsx
@@ -0,0 +1,18 @@
+import { Shimmer } from '@fluentui/react';
+import * as React from 'react';
+
+export default class NewJobPostingForm extends React.Component<any, any> {
+    public render(): React.ReactElement<any> {
+        let style = { padding: "5px" };
+        return (
+            <div>
+                <Shimmer style={style} />
+                <Shimmer style={style} width="75%" />
+                <Shimmer style={style} width="50%" />
+                <Shimmer style={style} />
+                <Shimmer style={style} width="75%" />
+                <Shimmer style={style} width="50%" />
+            </div>
+        );
+    }
+}
